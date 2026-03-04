e8c85f3692ac28384debebaeee8e7d2308b51df5
william <Willih@stud.ntnu.no>
2026-03-04 14:19:25 +0100

    Removed gradio impementatioon

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import gradio as gr  # Added this to access themes
+from nicegui import ui
 from src.product_manager import ProductManager
 from src.inventory_manager import InventoryManager
 from src.qr_generator import QRGenerator
 from src.alert_system import AlertSystem
 from src.scanner import QRScanner
 from src.opened_manager import OpenedManager
-from src.ui import create_app
+from src.ui import create_ui
 
 # 1. Initialize Backend Systems
 products = ProductManager()
@@ -16,9 +16,8 @@ alerts = AlertSystem()
 scanner = QRScanner()
 
 # 2. Build the UI
-app = create_app(products, inventory, opened, qr_gen, alerts, scanner)
+create_ui(products, inventory, opened, qr_gen, alerts, scanner)
 
 # 3. Run
-if __name__ == "__main__":
-    # The theme is moved here to satisfy the Gradio 6.0 requirement
-    app.launch(theme=gr.themes.Soft())
+if __name__ in {"__main__", "__mp_main__", "builtins"}:
+    ui.run(title="Modular Inventory System - NiceGUI", port=8080)
